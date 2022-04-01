94a8bd5a25f8550c74ee6866b93edb3fb8f22a6f
nik <channel.220volt@gmail.com>
2022-04-01 19:40:40 +1000

    Initial commit

---
+++ views/site/index.php
@@ -0,0 +1,66 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use app\assets\AppAsset;
+use yii\helpers\Html;
+
+//$this->title = 'About';
+//$this->params['breadcrumbs'][] = $this->title;
+
+?>
+
+<?//= \yii\helpers\Html::a('<i class="fas fa-2x fa-arrow-circle-left" style="color: #b6b6b6"></i>', ['create'], ['class' => 'btn']) ?>
+
+
+
+<div class="  ">
+    <div class="row">
+        <div class="col-xl-4">
+            <div class="index-container">
+                <a href="index">
+                    <img src="/images/car-order.jpg" alt="chechlop" class="index-images rounded-2 img-fluid">
+
+                    <div class="index-images-text-top px-3 ps-5 py-1">
+                        Заказ автомобилей
+                    </div>
+
+                    <!--                <a href="index" class="index-images-text-bottom index-images-text-bottom-first px-3 py-1">-->
+                    <!--                    Подробнее-->
+                    <!--                </a>-->
+                </a>
+
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="index-container">
+                <a href="">
+                    <img src="/images/auto-parts.jpg" alt="chechlop" class="index-images rounded-2 img-fluid">
+
+                    <div class="index-images-text-top px-3 ps-5 py-1">
+                        Магазин автозапчастей
+                    </div>
+                </a>
+
+
+            </div>
+
+        </div>
+
+        <div class="col-4">
+            <div class="index-container">
+                <img src="/images/repair1.1.jpg" alt="chechlop" class="index-images rounded-2 img-fluid">
+
+                <div class="index-images-text-top px-3 ps-5 py-1">
+                    Обслуживание и ремонт
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    <div class="row mt-3">
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+    </div>
+</div>
